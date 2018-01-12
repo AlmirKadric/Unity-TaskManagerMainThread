885928a086983298b3f3b43fe4d46d4fe76980e1
Almir Kadric <github@almirkadric.com>
2018-01-12 14:05:09 +0900

    added dependency class to repo

---
+++ MonoSingleton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace AlmirKadric.Utils
+{
+	public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		// Instance functions
+		protected static T instance;
+		public static T Instance {
+			get {
+				if (instance == null) {
+					Instantiate();
+				}
+
+				return instance;
+			}
+		}
+
+		// Instantiation function if you need to pre-instantiate rather than on demand
+		public static void Instantiate() {
+			if (instance != null) {
+				return;
+			}
+
+			var newObject = new GameObject(typeof(T).Name);
+			DontDestroyOnLoad(newObject);
+
+			instance = newObject.AddComponent<T>();
+		}
+
+		// Use this for initialization before any start methods are called
+		protected virtual void Awake() {
+			if (instance != null) {
+				DestroyImmediate(gameObject);
+				return;
+			}
+
+			instance = (T)(object)this;
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
--- TaskManagerMainThread.cs
+++ TaskManagerMainThread.cs
@@ -1,37 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class WaitingTask {
-	public IEnumerator c;
-	public bool autoStart;
+namespace AlmirKadric.Utils
+{
+	public class WaitingTask
+	{
+		public IEnumerator c;
+		public bool autoStart;
 
-	public WaitingTask(IEnumerator c, bool autoStart) {
-		this.c = c;
-		this.autoStart = autoStart;
-	}
-}
-
-public class TaskManagerMainThread : MonoSingleton<TaskManagerMainThread> {
-	private static List<WaitingTask> queuedTasks = new List<WaitingTask>();
-
-	// Add task to queue to be executed on main thread.
-	// This is thread safe as it gets a lock the queue list reference.
-	public static void Queue(IEnumerator c, bool autoStart = true) {
-		lock ((object)queuedTasks) {
-			queuedTasks.Add(new WaitingTask(c, autoStart));
+		public WaitingTask(IEnumerator c, bool autoStart) {
+			this.c = c;
+			this.autoStart = autoStart;
 		}
 	}
 
-	// Instantiate queued tasks and clear queue list.
-	// This is thread safe as it gets a lock to the queue list reference.
-	void Update () {
-		lock ((object)queuedTasks) {
-			foreach (WaitingTask task in queuedTasks) {
-				new Task(task.c, task.autoStart);
-			}
+	public class TaskManagerMainThread : MonoSingleton<TaskManagerMainThread>
+	{
+		private static List<WaitingTask> queuedTasks = new List<WaitingTask>();
 
-			queuedTasks.Clear();
+		// Add task to queue to be executed on main thread.
+		// This is thread safe as it gets a lock the queue list reference.
+		public static void Queue(IEnumerator c, bool autoStart = true) {
+			lock ((object)queuedTasks) {
+				queuedTasks.Add(new WaitingTask(c, autoStart));
+			}
+		}
+
+		// Instantiate queued tasks and clear queue list.
+		// This is thread safe as it gets a lock to the queue list reference.
+		void Update() {
+			lock ((object)queuedTasks) {
+				foreach (WaitingTask task in queuedTasks) {
+					new Task(task.c, task.autoStart);
+				}
+
+				queuedTasks.Clear();
+			}
 		}
 	}
 }
